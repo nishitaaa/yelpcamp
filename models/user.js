684bf7f089baafa61601034d96d993cd0774ce3b
Nishita Acharya <33816684+nishitaaa@users.noreply.github.com>
2020-06-24 21:31:36 -0400

    revert back to before trying password reset

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose")
 var UserSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
+    username: String,
     password: String
 });
 
